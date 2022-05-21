c403fb76f0d0e279685eeac91d8601f6af00d1eb
Nekst <chareef2555@gmail.com>
2022-05-21 21:13:33 +0700

    update 5/21/2022

--- index.ts
+++ index.ts
@@ -1,67 +1,87 @@
 import axios from "axios";
-var prompt = require("prompt");
-const url = 'https://api1.binance.com/api/v3/depth?symbol=BTCUSDT';
+
+const api ='https://api1.binance.com/api/v3/depth?symbol=BTCUSDT';
+
+let inprice : number = 290000;
+let amounttoken : number = 0;
 
 
-async function func() {
-  let response = await axios.get(url);
+axios.get(api).then(response =>{
+  const asks = response.data.asks;
 
-  let updat = await response.data.lastUpdateId; //เวลาที่อัพเดท
-  let Bids = await response.data.bids;
-  let Asks =await response.data.asks;
-  const ob = {};
-  let innum : Number = 29200;
-
-  //console.log(Bids.length)
- 
-  Bids.map((item : any) => {
-    //console.log(+item[0])
-    //console.log(innum)
-    if(+item[0]<=innum){
-      console.log(item[0])
-    }
+  asks.map((usdtAmount :any)=>{
+    calculateOutputAmount(usdtAmount);
   });
-  //console.log(ob);
 
+  console.log(`Amount Token buy : ${amounttoken}`)
 
-  let newbid:Array<number>;
+});
 
+function calculateOutputAmount(usdtAmount:any){
 
+  let price : number = +usdtAmount[0];
+  let btc : number = +usdtAmount[1];
 
+  console.log('Price USDT :',price)
+  console.log('Amount Token :',btc)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // let b = prompt("Enter second number: ");
-  // console.log(b)
-  
-  
-
-
-  function calculateOutputAmount() { //function คำนวณ USDT => BTC
-
+  if(inprice>(price*btc)){
+    amounttoken = btc+amounttoken;
+    inprice = inprice - (price*btc);
   }
-  calculateOutputAmount()
-}
-func()
+  else if (inprice<price){
+    let token : number = inprice/price;
+    if(btc >= token){
+      amounttoken = token+amounttoken;
+      inprice = 0;
+    }
+    else{
+      let lastprice : number = (inprice*btc)/token;
+      amounttoken = btc + amounttoken;
+      inprice = lastprice;
+    }
+  }
+  
 
+
+}
+
+//old ver for fix leter
+// import axios from "axios";
+// //var prompt = require("prompt");
+// const url = 'https://api1.binance.com/api/v3/depth?symbol=BTCUSDT';
+
+// let innum : number = 30000;
+// let tk : number = 0;
+
+
+// async function func() {
+//   let response = await axios.get(url);
+//   let updat = await response.data.lastUpdateId; //เวลาที่อัพเดท
+//   let Bids = await response.data.bids; //ข้อมูลราคาผู้ซื้อ
+//   let Asks =await response.data.asks; //ข้อมูลราคาผู้ขาย
+  
+//   //console.log(Bids.length) //แสดงจำนวนของข้อมูล Bids
+
+//   Bids.map((item : any) => {
+//     console.log('price',+item[0]) //แสดงราคา
+//     console.log('amount',+item[1]) //แสดงจำนวนเหรียญ
+//     calculateOutputAmount(item);
+//     // console.log(item)
+
+//   });
+//   console.log(`Output BTC: ${tk}`);
+
+//   function calculateOutputAmount(item:any){
+//     let price : number = parseFloat(item[0]);
+//     let token : number = parseFloat(item[1]);
+//     //console.log(p,t)
+//     if(innum > price){
+//       tk = token + tk;
+//       price= innum-price;
+//       console.log(price)
+//       console.log(token)
+//   }
+//   }
+// }
+// func()
